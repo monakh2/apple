8c7edb0861f579f507032d421454ef3137222af9
kmonastyrskiy <kmonastyrskiy@gmail.com>
2017-06-14 17:26:02 +0300

    add slider

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,1 +1,14 @@
-
+/* =================================
+ popover/clicover
+ ==================================== */
+$('.popover-markup>.trigger').clickover({
+	placement: 'bottom',
+	width: 270,
+	html: true,
+	title: function () {
+		return $(this).parent().find('.head').html();
+	},
+	content: function () {
+		return $(this).parent().find('.content').html();
+	}
+});
--- tars-config.js
+++ tars-config.js
@@ -19,7 +19,7 @@ module.exports = {
 	"js": {
 		"workflow": "concat",
 		"bundler": "webpack",
-		"lint": true,
+		"lint": false,
 		"useBabel": true,
 		"removeConsoleLog": true,
 		"webpack": {
