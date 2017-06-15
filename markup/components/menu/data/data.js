b5113bd65a40932d55071cc14314cf94ab07a419
kmonastyrskiy <kmonastyrskiy@gmail.com>
2017-06-15 16:43:31 +0300

    add advantages

---
+++ markup/components/menu/data/data.js
@@ -0,0 +1,30 @@
+var data = {
+	menu: {
+		links: {
+			linkA: {
+				title: "Apple iPhone",
+				class: "menu-icon-iphone"
+			},
+			linkB: {
+				title: "Apple iPad",
+				class: "menu-icon-ipad"
+			},
+			linkC: {
+				title: "Apple MacBook",
+				class: "menu-icon-macbook"
+			},
+			linkD: {
+				title: "Apple Watch",
+				class: "menu-icon-watch"
+			},
+			linkE: {
+				title: "Apple iMac",
+				class: "menu-icon-desktop"
+			},
+			linkF: {
+				title: "Accessories",
+				class: "menu-icon-keyboard"
+			}
+		}
+	}
+};
