f266e97954e71d1db8838669dab53536a216c0d6
kmonastyrskiy <kmonastyrskiy@gmail.com>
2017-07-04 08:56:31 +0300

    Version 04.07.2017 Before start working clickover

--- markup/components/menu/menu.js
+++ markup/components/menu/menu.js
@@ -1,51 +1,83 @@
-if ($(window).width() <= 768) {
+if ($(window).width() <= 768 -17 ) {
 	makePanels();
-	// moveToHeader();
+	moveToTop();
+
 } else {
 	makeDropdown();
-	// moveToSlider();
+	moveToBottom();
 }
 
 $(window).on('resizeend', function () {
-	if ($(window).width() <= 768) {
+	if ($(window).width() <= 768-17) {
 		makePanels();
-		// moveToHeader();
+		moveToTop();
 	} else {
 		makeDropdown();
-		// moveToSlider();
+		moveToBottom();
 	}
 });
 
 function makePanels() {
 	var mainMenu   = $('.main-menu');
 	var liDropdown = $('.main-menu > .dropdown');
-	mainMenu.removeClass('nav navbar-nav').removeAttr('data-hover').addClass('panel-group list-unstyled').attr('id', 'menu-accordion');
-	liDropdown.removeClass('dropdown').addClass('panel panel-default').find('.dropdown-toggle').each(function (index) {
-		$(this).wrap('<div class="panel-heading"></div>').removeClass('dropdown-toggle').addClass('accordion-toggle collapsed').attr('data-toggle', 'collapse').attr('data-parent', '#menu-accordion').removeAttr('role').attr('href', '#menu-collapse-' + index);
+
+	mainMenu.removeClass('nav navbar-nav')
+		.removeAttr('data-hover')
+		.addClass('panel-group list-unstyled')
+		.attr('id', 'menu-accordion');
+
+	liDropdown.removeClass('dropdown')
+		.addClass('panel panel-default')
+		.find('.dropdown-toggle').each(function (index) {
+		$(this).wrap('<div class="panel-heading"></div>')
+			.removeClass('dropdown-toggle')
+			.addClass('accordion-toggle collapsed')
+			.attr('data-toggle', 'collapse')
+			.attr('data-parent', '#menu-accordion')
+			.removeAttr('role')
+			.attr('href', '#menu-collapse-' + index);
 	});
+
 	liDropdown.find('.dropdown-menu').each(function (index) {
-		$(this).removeClass('dropdown-menu dropdownhover-bottom').addClass('list-unstyled').removeAttr('role').wrap('<div class="panel-collapse collapse" aria-expanded="false">').parent('.panel-collapse').attr('id', 'menu-collapse-' + index);
+		$(this).removeClass('dropdown-menu dropdownhover-bottom')
+			.addClass('list-unstyled')
+			.removeAttr('role')
+			.wrap('<div class="panel-collapse collapse" aria-expanded="false">')
+			.parent('.panel-collapse')
+			.attr('id', 'menu-collapse-' + index);
 	});
 }
 
 function makeDropdown() {
 	var mainMenu = $('.main-menu');
-	mainMenu.removeClass('panel-group list-unstyled').removeAttr('id').attr('data-hover', 'dropdown').addClass('nav navbar-nav').find('.panel').each(function (index) {
+	mainMenu.removeClass('panel-group list-unstyled')
+		.removeAttr('id')
+		.attr('data-hover', 'dropdown')
+		.addClass('nav navbar-nav')
+		.find('.panel').each(function (index) {
 		var list = $(this).find('ul.list-unstyled');
+		$(this).removeClass('panel panel-default')
+			.addClass('dropdown')
+			.find('.accordion-toggle')
+			.unwrap()
+			.attr('href', '#')
+			.removeClass('accordion-toggle collapsed')
+			.removeAttr('data-parent')
+			.addClass('dropdown-toggle')
+			.attr('role', 'button')
+			.attr('data-toggle', 'dropdown');
 
-		$(this).removeClass('panel panel-default').addClass('dropdown').find('.accordion-toggle').unwrap().attr('href', '#').removeClass('accordion-toggle collapsed').removeAttr('data-parent').addClass('dropdown-toggle').attr('role', 'button').attr('data-toggle', 'dropdown');
-		list.unwrap().addClass('dropdown-menu dropdownhover-bottom').removeClass('list-unstyled').attr('role', 'menu');
+		list.unwrap()
+			.addClass('dropdown-menu dropdownhover-bottom')
+			.removeClass('list-unstyled')
+			.attr('role', 'menu');
 	});
 }
 
-function moveToHeader() {
-	var pageMenu = $('#page-menu').remove();
-	$('#header').append(pageMenu);
-}
-function moveToSlider() {
-	$('#page-menu').insertBefore('#advantages');
-}
+function moveToTop() {
+	$('#page-menu').insertAfter($('#header'));
 
-function sayMyName() {
-
+}
+function moveToBottom() {
+	$('#page-menu').insertAfter($('#slider'));
 }
---
+++ markup/components/test/test.js
@@ -0,0 +1,17 @@
+// $('#moveToTop').click(function (e) {
+// 	e.preventDefault();
+// 	moveToTop();
+// });
+// $('#moveToBottom').click(function (e) {
+// 	e.preventDefault();
+// 	moveToBottom();
+// });
+
+
+// function moveToTop() {
+// 	$('#page-menu').insertAfter($('#header'));
+//
+// }
+// function moveToBottom() {
+// 	$('#page-menu').insertAfter($('#slider'));
+// }
